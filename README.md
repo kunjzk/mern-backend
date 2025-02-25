e1740835d042a8f18824b696893ef671b3ca8ab3
Kunal Katarya <kunal.rox@gmail.com>
2025-02-25 11:00:10 +0800

    simple express crud app

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ Backend project with MERN and Mongo
 
 - run server in vscode
 - use postman GET functionality to send requests
+- Create a collection that stores all your api calls
+  - Create a blank collection and name it
+  - Add new requests
+  - Select the url and set it to be a variable, scoped to the collection
+  - Make sure to save the request before creating a new one
 
 ## Express
 
@@ -15,3 +20,19 @@ Package.json is the entrypoint
 1. `npm init -y` asks questions about repo and creates package.json
 2. Create an `index.js` and create a start command that runs `node index.js`
 3. `npm install express` -- this will create node_modules and add a new section to package.json
+
+- Running a dev server with hot reload - `npm i -D nodemon` --> creates a dev dependency in package.json
+  - Add an entry to `package.json` under `scripts` called `dev`: `"dev": "nodemon index.js`, now run a dev server with hot realod using `npm run dev`
+
+## Receiving data from the frontend
+
+Express docs: https://expressjs.com/en/5x/api.html#req.body
+
+- `app.use(express.json())`
+- Data coming in via the request body is accessed using `req.body`
+- Data coming via the url is accessed via `req.params` -> this comes as a string to use `parseInt` as needed
+- Create placeholders for variables when declaring routes using `/path/:variable`
+- Return data with `res.status(code).send(json data)`
+- Filter data using `object.find(predicate, i.e (t) => t.id === req.body.id)`
+- Extract data from request body using curly brace notation for succinctness
+- Delete data from array by first finding its index (use `findIndex`) and then calling `splice`
--- express/index.js
+++ express/index.js
@@ -8,6 +8,64 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/ice-tea", (req, res) => {
+  res.send("Hello world from ice tea");
+});
+
+app.get("/hot-tea", (req, res) => {
+  res.send("Hello world from hot tea");
+});
+
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
+});
+
+app.use(express.json());
+
+let teaData = [];
+let nextId = 1;
+
+// C: Create
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = { id: nextId++, name, price }; // shorthand notation to create "name": <name>, etc
+  teaData.push(newTea);
+  res.status(201).send(newTea); // send json data
+});
+
+// R: Read
+app.get("/teas", (req, res) => {
+  res.status(200).send(teaData);
+});
+
+app.get("/teas/:id", (req, res) => {
+  const tea = teaData.find((t) => t.id === parseInt(req.params.id)); // any data coming from the body is accessed via req.body, anything coming from the url is accessed via params
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  res.status(200).send(tea);
+});
+
+// U: Update tea
+app.put("/teas/:id", (req, res) => {
+  const teaID = parseInt(req.params.id);
+  const tea = teaData.find((t) => t.id === teaID);
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.status(200).send(tea);
+});
+
+// D: Delete tea
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id == parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("tea not found");
+  }
+  teaData.splice(index, 1);
+  res.status(204).send(teaData);
 });
